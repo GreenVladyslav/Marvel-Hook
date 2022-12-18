4315a8ef48abe497c441e053b02b14b3b4e4334d
Vladyslav <jrlimueno@gmail.lcom>
2022-12-18 06:11:35 +0200

    update React RRouter v6+, page404

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,3 +2,5 @@ import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage"
 
 export {MainPage, ComicsPage};
+
+// Для оптимизации если вдруг у нас будет очень много страничек ( и + мы в app импортируем только один обьект, одной строкой)
